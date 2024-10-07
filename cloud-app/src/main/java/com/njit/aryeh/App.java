b614a3e0ad212104f4ca365b438b8b426ef971cd
Aryeh Golob <ag645@njit.edu>
2024-10-07 13:57:28 -0400

    test

--- cloud-app/src/main/java/com/njit/aryeh/App.java
+++ cloud-app/src/main/java/com/njit/aryeh/App.java
@@ -2,9 +2,13 @@ package com.njit.aryeh;
 
 import java.util.List;
 
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.auth.profile.ProfileCredentialsProvider;
-import com.amazonaws.regions.Regions;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
+import software.amazon.awssdk.services.s3.model.S3Object;
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+
 
 /**
  * Hello world!
@@ -12,11 +16,45 @@ import com.amazonaws.regions.Regions;
  */
 public class App {
 	public static void main(String[] args) {
-		Regions clientRegion = Regions.DEFAULT_REGION;
-		String bucketName = "*** Bucket name ***";
+		System.out.println("aws s3 app started ...");
+		
+		String bucketName = "njit-cs-643";
+        Region region = Region.US_EAST_1; 
 
-		System.out.println("aws.accessKeyId: " + System.getProperty("aws.accessKeyId"));
-		System.out.println("aws.secretAccessKey: " + System.getProperty("aws.secretAccessKey"));
+		// Create a ProfileCredentialsProvider that reads from the default credentials file
+        ProfileCredentialsProvider credentialsProvider = ProfileCredentialsProvider.create();
+        
+        System.out.println("provide: "+credentialsProvider.toString());
+
+        // Create an S3 client using the credentials provider
+        S3Client s3 = S3Client.builder()
+                .region(Region.US_EAST_1)
+                .credentialsProvider(credentialsProvider)
+                .build();
+        
+        S3Client s3Client = S3Client.builder()
+                .region(region)
+                .build();
+
+        ListObjectsV2Request request = ListObjectsV2Request.builder()
+                .bucket(bucketName)
+                .build();
+
+        ListObjectsV2Response response;
+        do {
+            response = s3Client.listObjectsV2(request);
+
+            for (S3Object object : response.contents()) {
+                System.out.println(object.key());
+            }
+
+            request = ListObjectsV2Request.builder()
+                    .bucket(bucketName)
+                    .continuationToken(response.nextContinuationToken())
+                    .build();
+        } while (response.isTruncated());
+
+        s3Client.close();
 	}
 
 }
