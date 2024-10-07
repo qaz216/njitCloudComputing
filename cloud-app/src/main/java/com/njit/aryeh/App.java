4e23eb036c8888b51b85dad766a8e8cdbfcd0be0
Aryeh Golob <ag645@njit.edu>
2024-10-07 12:46:10 -0400

    test

--- cloud-app/src/main/java/com/njit/aryeh/App.java
+++ cloud-app/src/main/java/com/njit/aryeh/App.java
@@ -2,11 +2,9 @@ package com.njit.aryeh;
 
 import java.util.List;
 
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
-import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
-import software.amazon.awssdk.services.s3.model.S3Object;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Regions;
 
 /**
  * Hello world!
@@ -14,20 +12,11 @@ import software.amazon.awssdk.services.s3.model.S3Object;
  */
 public class App {
 	public static void main(String[] args) {
-		String AWS_BUCKET = "njit-cs-643";
-		Region AWS_REGION = Region.US_EAST_1;
+		Regions clientRegion = Regions.DEFAULT_REGION;
+		String bucketName = "*** Bucket name ***";
 
-		S3Client s3Client = S3Client.builder().region(AWS_REGION).build();
-
-		ListObjectsV2Request listObjectsV2Request = ListObjectsV2Request.builder().bucket(AWS_BUCKET).build();
-		ListObjectsV2Response listObjectsV2Response = s3Client.listObjectsV2(listObjectsV2Request);
-
-		List<S3Object> contents = listObjectsV2Response.contents();
-
-		System.out.println("Number of objects in the bucket: " + contents.stream().count());
-		contents.stream().forEach(System.out::println);
-
-		s3Client.close();
+		System.out.println("aws.accessKeyId: " + System.getProperty("aws.accessKeyId"));
+		System.out.println("aws.secretAccessKey: " + System.getProperty("aws.secretAccessKey"));
 	}
 
 }
