c45d202e22487a8b41a271294be79f87ccde01ce
Aryeh Golob <ag645@njit.edu>
2024-10-08 11:33:45 -0400

    changes

---
+++ cloud-app/src/main/java/com/njit/aryeh/CarRecognition.java
@@ -0,0 +1,79 @@
+package com.njit.aryeh;
+
+import java.io.IOException;
+import java.util.List;
+
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.core.ResponseInputStream;
+import software.amazon.awssdk.core.SdkBytes;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.rekognition.RekognitionClient;
+import software.amazon.awssdk.services.rekognition.model.DetectLabelsRequest;
+import software.amazon.awssdk.services.rekognition.model.DetectLabelsResponse;
+import software.amazon.awssdk.services.rekognition.model.DetectTextRequest;
+import software.amazon.awssdk.services.rekognition.model.DetectTextResponse;
+import software.amazon.awssdk.services.rekognition.model.Image;
+import software.amazon.awssdk.services.rekognition.model.Label;
+import software.amazon.awssdk.services.rekognition.model.TextDetection;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
+import software.amazon.awssdk.services.s3.model.S3Object;
+
+public class CarRecognition {
+	private String bucketName = null;
+	public final Region REGION = Region.US_EAST_1;
+
+	public CarRecognition(String bucketName) {
+		this.bucketName = bucketName;
+	}
+
+	public void processImages() {
+		ProfileCredentialsProvider credentialsProvider = ProfileCredentialsProvider.create();
+		RekognitionClient rekClient = RekognitionClient.builder().credentialsProvider(credentialsProvider)
+				.region(Region.US_EAST_1).build();
+
+		S3Client s3Client = S3Client.builder().region(REGION).build();
+		ListObjectsV2Request request = ListObjectsV2Request.builder().bucket(bucketName).build();
+		try {
+			ListObjectsV2Response response;
+			do {
+				response = s3Client.listObjectsV2(request);
+
+				for (S3Object object : response.contents()) {
+					System.out.println(object.key());
+
+					GetObjectRequest getObjectRequest = GetObjectRequest.builder().bucket(bucketName).key(object.key())
+							.build();
+
+					ResponseInputStream<GetObjectResponse> responseBytes = s3Client.getObject(getObjectRequest);
+
+					byte[] bytes = responseBytes.readAllBytes();
+					System.out.println("Object bytes length: " + bytes.length);
+
+					SdkBytes sourceBytes = SdkBytes.fromByteArray(bytes);
+					Image souImage = Image.builder().bytes(sourceBytes).build();
+
+					DetectLabelsRequest detectLabelsRequest = DetectLabelsRequest.builder().image(souImage)
+							.maxLabels(10).build();
+
+					DetectLabelsResponse labelsResponse = rekClient.detectLabels(detectLabelsRequest);
+
+					List<Label> labels = labelsResponse.labels();
+					for (Label label : labels) {
+						System.out.println("label: " + label.name() + " - " + label.confidence());
+					}
+
+					request = ListObjectsV2Request.builder().bucket(bucketName)
+							.continuationToken(response.nextContinuationToken()).build();
+				}
+			} while (response.isTruncated());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
---
+++ cloud-app/src/main/java/com/njit/aryeh/RecognitionApp.java
@@ -0,0 +1,36 @@
+package com.njit.aryeh;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class RecognitionApp {
+	private Properties prop = null;
+
+	public RecognitionApp() {
+		this.prop = new Properties();
+		try {
+			this.prop.load(RecognitionApp.class.getClassLoader().getResourceAsStream("application.properties"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Recognition Application starting ...");
+		RecognitionApp app = new RecognitionApp();
+		String mode = app.getMode();
+		String bucketName = app.getBucketName();
+		System.out.println("mode = " + mode);
+		if(mode.equals("car_recognition")) {
+			CarRecognition carReco = new CarRecognition(bucketName);
+		}
+	}
+	
+	private String getMode() {
+		return this.prop.getProperty("app.mode");
+	}
+
+	private String getBucketName() {
+		return this.prop.getProperty("app.bucket");
+	}
+}
