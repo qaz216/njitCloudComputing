8839180d2d295ff4386f5b3b7b6cebcae872bcdd
Aryeh Golob <ag645@njit.edu>
2024-10-08 14:28:18 -0400

    changes

--- cloud-app/src/main/java/com/njit/aryeh/TextRecognition.java
+++ cloud-app/src/main/java/com/njit/aryeh/TextRecognition.java
@@ -19,25 +19,24 @@ public class TextRecognition {
 	}
 
 	public void receiveImages() {
-        try {
-        	
-        	GetQueueUrlResponse getQueueUrlResponse = sqsClient
-					.getQueueUrl(GetQueueUrlRequest.builder().queueName(queueName).build());
-        	String queueUrl = getQueueUrlResponse.queueUrl();
-        	System.out.println("queue url: "+queueUrl);
-            ReceiveMessageRequest receiveMessageRequest = ReceiveMessageRequest.builder()
-                    .queueUrl(queueUrl)
-                    .maxNumberOfMessages(5)
-                    .build();
-        	List<Message> messages = sqsClient.receiveMessage(receiveMessageRequest).messages();
-        	for(Message message : messages) {
-        		System.out.println("message: "+message.body());
-        	}
+		while (true) {
+			try {
+				GetQueueUrlResponse getQueueUrlResponse = sqsClient
+						.getQueueUrl(GetQueueUrlRequest.builder().queueName(queueName).build());
+				String queueUrl = getQueueUrlResponse.queueUrl();
+				System.out.println("queue url: " + queueUrl);
+				ReceiveMessageRequest receiveMessageRequest = ReceiveMessageRequest.builder().queueUrl(queueUrl)
+						.maxNumberOfMessages(5).build();
+				List<Message> messages = sqsClient.receiveMessage(receiveMessageRequest).messages();
+				for (Message message : messages) {
+					System.out.println("message: " + message.body());
+				}
 
-        } catch (SqsException e) {
-            System.err.println(e.awsErrorDetails().errorMessage());
-            System.exit(1);
-        }
+			} catch (SqsException e) {
+				System.err.println(e.awsErrorDetails().errorMessage());
+				System.exit(1);
+			}
+		}
 
 	}
 
