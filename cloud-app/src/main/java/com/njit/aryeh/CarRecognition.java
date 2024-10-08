ebc8a258245dcf0ac8fba3663228aafe0af2ee5e
Aryeh Golob <ag645@njit.edu>
2024-10-08 11:53:06 -0400

    changes

--- cloud-app/src/main/java/com/njit/aryeh/CarRecognition.java
+++ cloud-app/src/main/java/com/njit/aryeh/CarRecognition.java
@@ -10,11 +10,8 @@ import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.rekognition.RekognitionClient;
 import software.amazon.awssdk.services.rekognition.model.DetectLabelsRequest;
 import software.amazon.awssdk.services.rekognition.model.DetectLabelsResponse;
-import software.amazon.awssdk.services.rekognition.model.DetectTextRequest;
-import software.amazon.awssdk.services.rekognition.model.DetectTextResponse;
 import software.amazon.awssdk.services.rekognition.model.Image;
 import software.amazon.awssdk.services.rekognition.model.Label;
-import software.amazon.awssdk.services.rekognition.model.TextDetection;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.GetObjectResponse;
@@ -23,8 +20,8 @@ import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
 import software.amazon.awssdk.services.s3.model.S3Object;
 
 public class CarRecognition {
-	private String bucketName = null;
 	public final Region REGION = Region.US_EAST_1;
+	private String bucketName = null;
 	private ProfileCredentialsProvider credentialsProvider;
 	private RekognitionClient rekClient;
 	private S3Client s3Client;
@@ -32,35 +29,28 @@ public class CarRecognition {
 	public CarRecognition(String bucketName) {
 		this.bucketName = bucketName;
 		this.credentialsProvider = ProfileCredentialsProvider.create();
-		this.rekClient = RekognitionClient.builder().credentialsProvider(credentialsProvider)
+		this.rekClient = RekognitionClient.builder().credentialsProvider(this.credentialsProvider)
 				.region(Region.US_EAST_1).build();
 		this.s3Client = S3Client.builder().region(REGION).build();
 	}
 
 	public void processImages() {
-		System.out.println("got here ...");
-		/*
-		ProfileCredentialsProvider credentialsProvider = ProfileCredentialsProvider.create();
-		RekognitionClient rekClient = RekognitionClient.builder().credentialsProvider(credentialsProvider)
-				.region(Region.US_EAST_1).build();
-		S3Client s3Client = S3Client.builder().region(REGION).build();
-		*/
-		ListObjectsV2Request request = ListObjectsV2Request.builder().bucket(bucketName).build();
+		ListObjectsV2Request request = ListObjectsV2Request.builder().bucket(this.bucketName).build();
 		try {
 			ListObjectsV2Response response;
 			do {
-				response = s3Client.listObjectsV2(request);
+				response = this.s3Client.listObjectsV2(request);
 
 				for (S3Object object : response.contents()) {
-					System.out.println(object.key());
+					String key = object.key();
+					System.out.println("processing image: "+key);
 
-					GetObjectRequest getObjectRequest = GetObjectRequest.builder().bucket(bucketName).key(object.key())
+					GetObjectRequest getObjectRequest = GetObjectRequest.builder().bucket(this.bucketName).key(object.key())
 							.build();
 
-					ResponseInputStream<GetObjectResponse> responseBytes = s3Client.getObject(getObjectRequest);
+					ResponseInputStream<GetObjectResponse> responseBytes = this.s3Client.getObject(getObjectRequest);
 
 					byte[] bytes = responseBytes.readAllBytes();
-					System.out.println("Object bytes length: " + bytes.length);
 
 					SdkBytes sourceBytes = SdkBytes.fromByteArray(bytes);
 					Image souImage = Image.builder().bytes(sourceBytes).build();
@@ -68,14 +58,19 @@ public class CarRecognition {
 					DetectLabelsRequest detectLabelsRequest = DetectLabelsRequest.builder().image(souImage)
 							.maxLabels(10).build();
 
-					DetectLabelsResponse labelsResponse = rekClient.detectLabels(detectLabelsRequest);
+					DetectLabelsResponse labelsResponse = this.rekClient.detectLabels(detectLabelsRequest);
 
 					List<Label> labels = labelsResponse.labels();
 					for (Label label : labels) {
-						System.out.println("label: " + label.name() + " - " + label.confidence());
+						String labelName = label.name().toLowerCase().trim();
+						Float confidence = label.confidence();
+						if(labelName.equals("car") &&  confidence >= 90.0) {
+							System.out.println("Found a car with confidence over 90%");
+							System.out.println("image name: " + key + " label: " + labelName + " confidence: " + confidence);
+						}
 					}
 
-					request = ListObjectsV2Request.builder().bucket(bucketName)
+					request = ListObjectsV2Request.builder().bucket(this.bucketName)
 							.continuationToken(response.nextContinuationToken()).build();
 				}
 			} while (response.isTruncated());
@@ -84,5 +79,4 @@ public class CarRecognition {
 		}
 
 	}
-
 }
