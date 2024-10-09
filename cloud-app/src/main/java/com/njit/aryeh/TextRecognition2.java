8d93ede91d18b351030e39cb20815a7744b73b07
Aryeh Golob <ag645@njit.edu>
2024-10-09 11:40:18 -0400

    checkin

---
+++ cloud-app/src/main/java/com/njit/aryeh/TextRecognition2.java
@@ -0,0 +1,60 @@
+package com.njit.aryeh;
+
+import java.util.List;
+
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.DeleteMessageRequest;
+import software.amazon.awssdk.services.sqs.model.GetQueueUrlRequest;
+import software.amazon.awssdk.services.sqs.model.GetQueueUrlResponse;
+import software.amazon.awssdk.services.sqs.model.Message;
+import software.amazon.awssdk.services.sqs.model.ReceiveMessageRequest;
+import software.amazon.awssdk.services.sqs.model.SqsException;
+
+public class TextRecognition2 {
+	private SqsClient sqsClient;
+	private String queueName;
+
+	public TextRecognition2(SqsClient sqsClient, String queueName) {
+		this.sqsClient = sqsClient;
+		this.queueName = queueName;
+	}
+
+	public void receiveImages() {
+		boolean exitLoop = false;
+		while (true) {
+			try {
+				GetQueueUrlResponse getQueueUrlResponse = sqsClient
+						.getQueueUrl(GetQueueUrlRequest.builder().queueName(queueName).build());
+				String queueUrl = getQueueUrlResponse.queueUrl();
+				System.out.println("queue url: " + queueUrl);
+				ReceiveMessageRequest receiveMessageRequest = ReceiveMessageRequest.builder().queueUrl(queueUrl)
+						.maxNumberOfMessages(5).build();
+				List<Message> messages = sqsClient.receiveMessage(receiveMessageRequest).messages();
+				for (Message message : messages) {
+					String messageBody = message.body();
+					System.out.println("message: " + messageBody);
+					DeleteMessageRequest deleteMessageRequest = DeleteMessageRequest.builder()
+                            .queueUrl(queueUrl)
+                            .receiptHandle(message.receiptHandle())
+                            .build();
+                    sqsClient.deleteMessage(deleteMessageRequest);
+					if(messageBody.equals("-1")) {
+						System.out.println("-1 received ... exiting");
+						exitLoop = true;
+						break;
+					}
+				}
+				if(exitLoop) {
+					System.out.println("exiting loop");
+					break;
+				}
+
+			} catch (SqsException e) {
+				System.err.println(e.awsErrorDetails().errorMessage());
+				System.exit(1);
+			}
+		}
+
+	}
+
+}
